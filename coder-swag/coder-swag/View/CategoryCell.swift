32e20c5635a084bfc2c1147a1881da3862d07e1b
Nayan Jariwala <nayanjariwala@Nayans-iMac.local>
2018-03-23 09:28:17 +0530

    first commit

---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Nayan Jariwala on 23/03/18.
+//  Copyright © 2018 Nayan Jariwala. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
