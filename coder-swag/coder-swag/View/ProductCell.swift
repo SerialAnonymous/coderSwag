f0f336e525ae5e4b0fa0440934867b390f763444
Nayan Jariwala <nayanjariwala@Nayans-iMac.local>
2018-03-26 08:26:28 +0530

    fourth commit

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Nayan Jariwala on 25/03/18.
+//  Copyright © 2018 Nayan Jariwala. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
